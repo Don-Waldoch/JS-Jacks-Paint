be280ce6def74a619a1e08e0cc0258a6e195f31b
unknown <waldoch.don@gmail.com>
2022-02-01 20:55:26 -0600

    My Spin

--- assets/js/paint.js
+++ assets/js/paint.js
@@ -1,98 +1,39 @@
 function configureListeners() {
-    var images = document.getElementsByTagName('img');    
-
-     for (var i = 0; i < images.length; i++) {        
-        document.getElementById(images[i].id).addEventListener('mouseover', addOpacity, false)        
-        document.getElementById(images[i].id).addEventListener('mouseout', removeOpacity, false)        
-    } 
+    let images = document.getElementsByTagName('img');
+    for (let i = 0; i < images.length; i++) {
+        document.getElementById(images[i].id).addEventListener('mouseenter', addOpacity, false);
+        document.getElementById(images[i].id).addEventListener('mouseleave', removeOpacity, false);
+    }
 }
 
-function addOpacity(event) {
-    if (!this.classList.contains('dim')){
-        this.classList.add('dim')
-    }    
-    getProductInfo(event.target.id);     
+function addOpacity(e) {
+    if (!this.classList.contains('dim')) {
+        this.classList.add('dim');
+    }
+    putProductInfo(this.id);
 }
 
-function removeOpacity(event) {
-     if (this.classList.contains('dim')){
+function removeOpacity(e) {
+    if (this.classList.contains('dim')) {
         this.classList.remove('dim');
     }
-
-    let element = document.getElementById('ppg');
-        element.textContent = '';
-        
-    let color = document.getElementById('color');
-        color.textContent = ''; 
-
-    event.preventDefault();    
+    putProductInfo('');
 }
 
-function changeImage(elementId) {
-    let image = document.getElementById('imgDisplay');
-    image.src = elementId.src;
+let ProductInfo = {
+    pn1: {price: '$19.99', color: 'Lime Green'},
+    pn2: {price: '$12.99', color: 'Medium Brown'},
+    pn3: {price: '$11.99', color: 'Royal Blue'},
+    pn4: {price: '$14.99', color: 'Bright Red'},
+    pn5: {price:  '$9.99', color: 'Solid White'},
+    pn6: {price: '$15.99', color: 'Solid Black'},
+    pn7: {price:  '$8.99', color: 'Medium Blue'},
+    pn8: {price: '$16.99', color: 'Light Purple'},
+    pn9: {price: '$17.99', color: 'Bright Yellow'},
+    '': {price: '', color: ''}
 }
 
-function getProductInfo(partNumber) {
-    let price;
-    let colorName;  
-    
-    switch (partNumber) {
-        case 'pn1':           
-            price = '$19.99'
-            colorName = 'Lime Green'
-            updatePrice(colorName, price)           
-            break;           
-        case 'pn2':
-            price = '$12.99'
-            colorName = 'Medium Brown' 
-            updatePrice(colorName, price)     
-            break;            
-        case 'pn3':
-            price = '$11.99'
-            colorName = 'Royal Blue'   
-            updatePrice(colorName, price)   
-            break;   
-        case 'pn4':
-            price = '$14.99'
-            colorName = 'Bright Red'            
-            updatePrice(colorName, price)   
-            break;   
-        case 'pn5':
-            price = '$9.99'
-            colorName = 'Solid White' 
-            updatePrice(colorName, price)              
-            break;   
-        case 'pn6':
-            price = '$15.99'
-            colorName = 'Solid Black'   
-            updatePrice(colorName, price)            
-            break;   
-        case 'pn7':
-            price = '$8.99'
-            colorName = 'Medium Blue'       
-            updatePrice(colorName, price)        
-            break;   
-        case 'pn8':
-            price = '$16.99'
-            colorName = 'Light Purple'      
-            updatePrice(colorName, price)         
-            break;   
-        case 'pn9':
-            price = '$17.99'
-            colorName = 'Bright Yellow'                       
-            updatePrice(colorName, price)   
-            break;   
-          default:              
-    }
-
-    function updatePrice(colorName, price)
-    {       
-        let ppg = document.getElementById('ppg');
-        ppg.textContent = price;
-        
-        let color = document.getElementById('color');
-        color.textContent = colorName;
-    }
-    
+function putProductInfo(partNumber) {
+    document.getElementById('ppg').textContent   = ProductInfo[partNumber].price;
+    document.getElementById('color').textContent = ProductInfo[partNumber].color;
 }
