04c3b29b82f4fb78ce21d928cd78ccb1f8ee26a8
unknown <waldoch.don@gmail.com>
2022-02-01 20:59:25 -0600

    My Spin Take2

--- assets/js/paint.js
+++ assets/js/paint.js
@@ -1,3 +1,16 @@
+let ProductInfo = {
+    pn1: {price: '$19.99', color: 'Lime Green'   },
+    pn2: {price: '$12.99', color: 'Medium Brown' },
+    pn3: {price: '$11.99', color: 'Royal Blue'   },
+    pn4: {price: '$14.99', color: 'Bright Red'   },
+    pn5: {price:  '$9.99', color: 'Solid White'  },
+    pn6: {price: '$15.99', color: 'Solid Black'  },
+    pn7: {price:  '$8.99', color: 'Medium Blue'  },
+    pn8: {price: '$16.99', color: 'Light Purple' },
+    pn9: {price: '$17.99', color: 'Bright Yellow'},
+    '': {price: '', color: ''}
+}
+
 function configureListeners() {
     let images = document.getElementsByTagName('img');
     for (let i = 0; i < images.length; i++) {
@@ -18,19 +31,6 @@ function removeOpacity(e) {
         this.classList.remove('dim');
     }
     putProductInfo('');
-}
-
-let ProductInfo = {
-    pn1: {price: '$19.99', color: 'Lime Green'},
-    pn2: {price: '$12.99', color: 'Medium Brown'},
-    pn3: {price: '$11.99', color: 'Royal Blue'},
-    pn4: {price: '$14.99', color: 'Bright Red'},
-    pn5: {price:  '$9.99', color: 'Solid White'},
-    pn6: {price: '$15.99', color: 'Solid Black'},
-    pn7: {price:  '$8.99', color: 'Medium Blue'},
-    pn8: {price: '$16.99', color: 'Light Purple'},
-    pn9: {price: '$17.99', color: 'Bright Yellow'},
-    '': {price: '', color: ''}
 }
 
 function putProductInfo(partNumber) {
